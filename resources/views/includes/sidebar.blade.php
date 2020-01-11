927f7e61e23632b983e71fe1d61eea8f55d17d23
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-01-11 20:17:56 +0800

    Refactored sidebar script and app-layout

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,108 +1,79 @@
 <?php $auth = Auth::user(); ?>
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
-            <div class="container">
-                <div id="main">
-                    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
-                  </div>
-                <a class="navbar-brand" href="{{ url('/dashboard') }}">
-                    {{ config('app.name', 'EMR') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-        
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav mr-auto">
-                        
-                        
-                    </ul>
-        
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <img src="/img/Picture1.png" alt="Avatar" class="avatar">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name . " " . Auth::user()->last_name }} <span class="caret"></span>
-                                </a>
-        
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-        
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
+    <div class="container">
+        <div id="main">
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
             </div>
+        <a class="navbar-brand" href="{{ url('/dashboard') }}">
+            {{ config('app.name', 'EMR') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <!--sidebar content-->
-        <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>            
-            <div class="prof">
-                <img src="/img/Picture1.png" alt="Avatar" class="profile"><br>
-                <h4 style="color:white">Anne Hayathi Albiso</h4>
-                <p class="title" style="color:gray">Admin</p>
-            </div>
-            <a href="#"><i class="fa fa-dashboard"></i> Dashboard</a>
-            <a href="#"><i class="fa fa-cogs"></i> Services</a>
-            <a href="#"><i class="fa fa-user"></i> Account</a>
-            <a href="#"><i class="fa fa-phone"></i> Contact</a>
-          </div>
-          <!--end of side bar content-->
-          
-          <!--Active Users-->
-          <button class="open-button" onclick="openForm()"> <i class="fa fa-check"></i>Active Users</button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+            </ul>
 
-            <div class="chat-popup" id="myForm">
-            <form action="/action_page.php" class="form-container">
-                <img src="/img/Picture1.png" alt="Avatar" class="avatar2"> Anne Albiso <br><br>
-                <img src="/img/p1.png" alt="Avatar" class="avatar2"> Isnaina Abdulazis<br>
-               
-                <!--<label for="msg"><b>Message</b></label>
-                     -->
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <img src="/img/Picture1.png" alt="Avatar" class="avatar">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->first_name . " " . Auth::user()->last_name }} <span class="caret"></span>
+                        </a>
 
-                <!-- <button type="submit" class="btn">Send</button>-->
-                <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-            </form>
-            </div>
-            <!--End for Active Users-->
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-            <script>
-            function openForm() {
-            document.getElementById("myForm").style.display = "block";
-            }
-
-            function closeForm() {
-            document.getElementById("myForm").style.display = "none";
-            }
-            
-          
-          
-            function openNav() {
-                document.getElementById("mySidenav").style.width = "250px";
-            // document.getElementById("main").style.marginLeft = "250px";
-                document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-            }
-            
-            function closeNav() {
-                document.getElementById("mySidenav").style.width = "0";
-                document.getElementById("main").style.marginLeft= "0";
-                document.body.style.backgroundColor = "white";
-            }
-            </script>
-            </nav>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
-    <!-- /#wrapper -->
+
+    <!--sidebar content-->
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>            
+        <div class="prof">
+            <img src="/img/Picture1.png" alt="Avatar" class="profile"><br>
+            <h4 style="color:white">Anne Hayathi Albiso</h4>
+            <p class="title" style="color:gray">Admin</p>
+        </div>
+        <a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a>
+        <a href="#"><i class="fa fa-cogs"></i> Services</a>
+        <a href="#"><i class="fa fa-user"></i> Account</a>
+        <a href="#"><i class="fa fa-phone"></i> Contact</a>
+    </div>
+    <!--end of side bar content-->
+    
+    <!--Active Users-->
+    <button class="open-button" onclick="openForm()"> <i class="fa fa-check"></i>Active Users</button>
+    <div class="chat-popup" id="myForm">
+        <form action="/action_page.php" class="form-container">
+            <img src="/img/Picture1.png" alt="Avatar" class="avatar2"> Anne Albiso <br><br>
+            <img src="/img/p1.png" alt="Avatar" class="avatar2"> Isnaina Abdulazis<br>
+        
+            <!--<label for="msg"><b>Message</b></label>
+                -->
+
+            <!-- <button type="submit" class="btn">Send</button>-->
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+    </div>
+    <!--End for Active Users-->
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,8 @@
     <link href="{{ asset('css/userform.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
     <div id="app">
@@ -27,16 +24,39 @@
             @yield('content')
         </main>
         </div>
+
+        <div class="footer">
+            <p>Electronic Medical Record <i class="fa fa-copyright"> 2020</i></p>
+        </div>
     </div>
 
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
+    function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    }
+    
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        document.body.style.backgroundColor = "white";
+    }
     </script>
-    <div class="footer">
-        <p>Electronic Medical Record <i class="fa fa-copyright"> 2020</i></p>
-    </div>
 </body>
 </html>
