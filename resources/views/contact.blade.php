f4cb81fcf63587b78c039795b6b40bebe2dbbcd7
isnieeeee <isnieabdulazis@gmail.com>
2020-04-29 22:24:52 +0800

    I have implemented contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,112 +1,145 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card text-left">
-    <div class="card-header">
-    
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-html {
+* {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
-  box-sizing: inherit;
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
+/* The grid: Three equal columns that floats next to each other */
 .column {
   float: left;
   width: 50%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
-  }
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-}
-
-.container {
-  padding: 0 16px;
-}
-
-.container::after, .row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-.title {
-  color: grey;
-}
-
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
+  padding: 50px;
   text-align: center;
+  font-size: 25px;
   cursor: pointer;
-  width: 100%;
+  color: white;
 }
 
-.button:hover {
-  background-color: #555;
+.containerTab {
+  padding: 20px;
+  color: white;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Closable button inside the container tab */
+.closebtn {
+  float: right;
+  color: white;
+  font-size: 35px;
+  cursor: pointer;
 }
 </style>
 </head>
 <body>
 
-<h2><center>"About the Developers"</center></h2>
-<p><center>We are the developer of the EMR System for MSU-IIT Clinic</center></p>
-<br>
+<div style="text-align:center">
+  <h1>CONTACT INFORMATION</h1>
+  <h5><em>Click on the boxes below:</em></h5>
+</div>
 
+<!-- Three columns -->
 <div class="row">
-  <div class="col-md-6">
-    <div class="card">
-    <img src="img/2x2a.jpg" alt="image" class="center" style="width:300px;height:300px;" >
-      <div class="container">
-        <h2>Anne Hayathi S. Albiso</h2>
-        <p class="title">EMR System Developer</p>
-        <p>Course: BS Information Technology - Multimedia</p>
-        <p>E-mail Address: annehayathi1@gmail.com</p>
-        <p>Phone Number: 0915-565-0790 / 0927-549-0750</p>
-        <p>facebook link: https://www.facebook.com/anneyourlife.anneong</p>
-       
-      </div>
-    </div>
+  <div class="column w3-padding-64 w3-opacity w3-hover-opacity-off" onclick="openTab('b1');" style="background: linear-gradient(#AED6F1,#3498DB);">
+    CLINIC CONTACT INFORMATION
   </div>
-
-  <div class="col-md-6">
-    <div class="card">
-    <img src="img/2x2.jpg" alt="image" class="center" style="width:300px;height:300px;">
-      <div class="container">
-      <h2>Isnaina U. Abdulazis</h2>
-        <p class="title">EMR System Developer</p>
-        <p>Course: BS Information Technology - Multimedia</p>
-        <p>E-mail Address: isnainaabdulazis@gmail.com</p>
-        <p>Phone Number: 0910-041-1645 / 0927-499-3905</p>
-        <p>facebook link: https://www.facebook.com/ISNIE05</p>
-        
-      </div>
-    </div>
-  </div>
-  
-
-</body>
-</html>
-
+  <div class="column w3-padding-64 w3-opacity w3-hover-opacity-off" onclick="openTab('b2');" style="background: linear-gradient(#F2D7D5,#C0392B);">
+    DEVELOPERS CONTACT INFORMATION
   </div>
 </div>
+
+<!-- Full-width columns: (hidden by default) -->
+<br><br>
+<div id="b1" class="containerTab" style="display:none;background:#273746 ;">
+  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+  <h1><center>CLINIC CONTACT INFORMATION</center></h1>
+  <br>
+
+  <div class="row">
+  <div class="column" style="background-color:#273746;">
+  <h4 style="text-align:left;">MSU-ILIGAN INSTITUTE OF TECHNOLOGY</h4>
+  <p style="text-align:left;"><small>Website: www.msuiit.edu.ph</small></p>
+  <p style="text-align:left;"><small>Telephone # +63 (063) 221-4056</small></p>
+  </div>
+  <div class="column" style="background-color:#273746;">
+  <h4 style="text-align:left;">INSTITUTE CLINIC</h4>
+  <p style="text-align:left;"><small>Telephone # 4444 (local)</small></p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column" style="background-color:#273746;">
+  <h4 style="text-align:left;">REGISTRAR</h4>
+  <p style="text-align:left;"><small>Email address: registrar@g.msuiit.edu.ph</small></p>
+  <p style="text-align:left;"><small>Telephone # +63 (063) 223 3794</small></p>
+  </div>
+  <div class="column" style="background-color:#273746;">
+  <h4 style="text-align:left;">ADMISSIONS</h4>
+  <p style="text-align:left;"><small>Email address: admissions@g.msuiit.edu.ph</small></p>
+  <p style="text-align:left;"><small>Telephone # +63 (063) 223 8641</small></p>
+  </div>
+</div>
+
+  </div>
+
+
+<div id="b2" class="containerTab" style="display:none;background:#7B241C ;">
+  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+  <h1><center>DEVELOPERS CONTACT INFORMATION<center></h1>
+  <br>
+   
+  <div class="row">
+  <div class="column" style="background-color:#7B241C;">
+  <h2>Anne Hayathi S. Albiso</h2>   
+  <hr style="width:100%;text-align:left;margin-left:0">                                         
+        <p class="title"><small><em>EMR System Developer</em></small></p>
+        <p style="text-align:left;"><small>Course: BS Information Technology - Multimedia</small></p>
+        <p style="text-align:left;"><small>E-mail Address: annehayathi1@gmail.com</small></p>
+        <p style="text-align:left;"><small>Phone Number: 0915-565-0790 / 0927-549-0750</small></p>
+        <p style="text-align:left;"><small>facebook link: https://www.facebook.com/anneyourlife.anneong</small></p>
+  </div>
+
+  <div class="column" style="background-color:#7B241C;">
+  <h2>Isnaina U. Abdulazis</h2>
+  <hr style="width:100%;text-align:left;margin-left:0">
+        <p class="title"><small><em>EMR System Developer</em></small></p>
+        <p style="text-align:left;"><small>Course: BS Information Technology - Multimedia</small></p>
+        <p style="text-align:left;"><small>E-mail Address: isnainaabdulazis@gmail.com</small></p>
+        <p style="text-align:left;"><small>Phone Number: 0910-041-1645 / 0927-499-3905</small></p>
+        <p style="text-align:left;"><small>facebook link: https://www.facebook.com/ISNIE05</small></p>
+  </div>
+</div>
+
+  </div>
+
+<script>
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+</script>
+
+</body>
+</html> 
+
 @stop
